1c72159e5cd98246983013f7fdd8d10a89042cb6
Markiian Simkiv <markiian_simkiv@epam.com>
2018-10-01 12:53:15 +0300

    UnionFind: QuickUnionWeighted.

---
+++ UnionFind/QuickUnionWeighted.js
@@ -0,0 +1,42 @@
+module.exports = class QuickUnionWeighted {
+  constructor(N) {
+    this.id = [];
+    this.size = [];
+
+    for (let i = 0; i < N; i++) {
+      this.id.push(i);
+      this.size.push(1);
+    }
+  }
+
+  root(p) {
+    let i = p;
+
+    while (i !== this.id[i]) {
+      i = this.id[i];
+    }
+
+    return i;
+  }
+
+  connected(p1, p2) {
+    return this.root(p1) === this.root(p2);
+  }
+
+  union(p1, p2) {
+    const i = this.root(p1);
+    const j = this.root(p2);
+
+    if (i === j) {
+      return;
+    }
+
+    if (this.size[i] < this.size[j]) {
+      this.id[i] = j;
+      this.size[j] += this.size[i];
+    } else {
+      this.id[j] = i;
+      this.size[i] += this.size[j];
+    }
+  }
+};
